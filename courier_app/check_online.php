50914a8427b234a5517ddb53450d05f755287e31
komaroff <57409850+komar0ffDes@users.noreply.github.com>
2021-05-30 17:46:53 +0300

    add: check online courier

---
+++ courier_app/check_online.php
@@ -0,0 +1,39 @@
+<?php
+require '../db/config.php';
+
+if ( empty($_GET['courier_id'])) { // check GET data
+    die(http_response_code(404));
+}
+else {
+    $courier_id = $_GET['courier_id'];
+}
+
+function isOnlineCourier($courier_id) {
+    $pdo = getPdo();
+    $query = $pdo->query("SELECT `courier_id`, `is_online` FROM `couriers` WHERE courier_id = '$courier_id' AND is_online = 1;"); // выполнение sql запроса
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+function getCourierWorkShift($courier_id) {
+    $pdo = getPdo();
+    $query = $pdo->query("SELECT `work_shifts`.`start_work_shift`, `work_shifts`.`end_work_shift` FROM `work_shifts` 
+    WHERE `work_shifts`.`work_shift_id` =  (SELECT MAX(`work_shift_id`) AS 'ID' FROM `work_shifts` WHERE `work_shifts`.`courier_id` = '$courier_id')"); // выполнение sql запроса
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+if (isOnlineCourier($courier_id)) {
+
+    $work_shift = getCourierWorkShift($courier_id);
+
+    $response = array(
+        "result" =>$work_shift,
+    );
+
+    print_r(json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+} else {
+    $response = array(
+        "result" =>false,
+    );
+
+    print_r(json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+}
