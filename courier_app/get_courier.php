ab1fbf463df98fd54bfe5c82d713b64aadc61160
komaroff <57409850+komar0ffDes@users.noreply.github.com>
2021-06-06 22:30:08 +0300

    add get_courier

---
+++ courier_app/get_courier.php
@@ -0,0 +1,28 @@
+<?php
+require '../db/config.php';
+
+if ( empty($_GET['courier_id']) ) { // check GET data
+    die(http_response_code(404));
+}
+else {
+    $courier_id = $_GET['courier_id'];
+}
+
+function getCourier($courier_id) { // берем данные заказов из БД по user_id
+    $pdo = getPdo();
+    $query = $pdo->query(
+        "SELECT `couriers`.`courier_phone`, `couriers`.`courier_name`, `couriers`.`all_shifts`, `couriers`.`all_hours`,
+        (SELECT COUNT(`order_id`) FROM `orders` WHERE `orders`.`courier_id`= '$courier_id' AND `orders`.`order_status_id`= 5) AS `all_orders`,
+        (SELECT SUM(`courier_salary`) FROM `orders` WHERE `orders`.`courier_id`= '$courier_id' AND `orders`.`order_status_id`= 5) AS `all_salary`
+        FROM `couriers` WHERE `courier_id` = 8;");
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+$courier_info = getCourier($courier_id);
+
+$response = array(
+    "result" =>$courier_info
+);
+
+
+print_r(json_encode($response, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
